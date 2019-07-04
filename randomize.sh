c516c5be60fbea336d4c7fb5fd52a1d21d0dd031
mrobert88 <mrobert88@gmail.com>
2019-07-04 15:05:01 +0200

    Automatic commit at 2019-07-04 15:05:01

---
+++ randomize.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function checkNumber {
+    if [ $1 -gt $2]; then
+	echo "The secret number is smaller!"
+    elif [$1 -lt $2]: then
+	echo "The secret number is greater!"
+    fi
+	
+}
+
+number=$((1 + RANDOM % 10))
+
+read -p "Guess the number:" $guess
+
+while [ $guess !-eq $number ]; do
+    checknumber $guess
+    read -p "Guess the number:" $guess
+done
+
+echo "Congrats! You`ve guessed the number!"
