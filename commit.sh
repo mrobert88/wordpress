ba596261e51b76dcb264350bbc4a7ae5e0d52d62
mrobert88 <mrobert88@gmail.com>
2019-07-04 13:20:01 +0200

    Automatic commit at 2019-07-04 13:20:01

--- commit.sh
+++ commit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #echo "Commit at $(date +"%Y-%m-%d %T")"
 cd /var/www/html/wordpress
+
 branch=$(git status | grep "On branch"| cut -d " " -f 4)
+echo $branch
 
 git add .
 git commit -m "Automatic commit at $(date +"%Y-%m-%d %T")"
