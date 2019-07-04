be6b7f889ae5c36081e074a4632875461229fe51
mrobert88 <mrobert88@gmail.com>
2019-07-04 03:12:51 -0400

    Automatic commit at 2019-07-04 03:12:51

--- commit.sh
+++ commit.sh
@@ -2,5 +2,5 @@
 #echo "Commit at $(date +"%Y-%m-%d %T")"
 cd /var/www/html/wordpress
 git add .
-git commit -m "Automatic commit at $(date + "%Y-%m-%d %T")"
-git push origin masteri
+git commit -m "Automatic commit at $(date +"%Y-%m-%d %T")"
+git push origin master
